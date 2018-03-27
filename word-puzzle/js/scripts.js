d36509c3c7896c10151b0f8ca75986a938231d20
Cameron Button and Kayl Eubanks <cameron@>
2018-03-27 16:44:03 -0700

    edit word-puzzle

--- word-puzzle/js/scripts.js
+++ word-puzzle/js/scripts.js
@@ -2,10 +2,14 @@ var vowels = ['a', 'e', 'o', 'i', 'u', 'A', 'E', 'I', 'O', 'U'];
 var stringToHide = "";
 var stringToAnswer = "";
 
+// here es yeer pezzle
+
+
 $(document).ready(function() {
   $('#form').submit(function(event){
     // debugger
     event.preventDefault();
+
     stringToHide = $('input#hidden-string').val();
     stringToAnswer = stringToHide;
     for (var i = 0; i < vowels.length; i++) {
@@ -17,20 +21,27 @@ $(document).ready(function() {
       }
     }
     $('#hidden-string').hide();
-    $('#puzzle').append(stringToHide);
+    $('#puzzle').text(stringToHide);
     $('#puzzle').show();
     $('.guess').show();
   });
   $('.guess').submit(function(event){
     event.preventDefault();
+       debugger;
     var userAnswer = $('#answer').val();
-    if (userAnswer){
-      console.log(userAnswer);
-      if (userAnswer === stringToAnswer){
-        $("#printAnswer").text("Good job !");
-      } else {
-        $("#printAnswer").text("Silly rabbit! Try again.");
+    stringToHide = stringToHide.replace(stringToHide.charAt(1), stringToAnswer.charAt(2));
+    console.log(stringToHide);
+    for (var i = 0; i <= userAnswer.length; i++) {
+      if (userAnswer.charAt(i) === stringToAnswer.charAt(i)) {
+        stringToHide = stringToHide.replace(stringToHide.charAt(i), stringToAnswer.charAt(i));
+        console.log(stringToHide);
       }
     }
-  })
+    $('#puzzle').text(stringToHide);
+    if (userAnswer === stringToAnswer) {
+      $("#printAnswer").text("Good job!");
+    } else {
+      $("#printAnswer").text("So close! Keep trying.");
+    }
+  });
 });
